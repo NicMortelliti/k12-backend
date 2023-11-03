d95afc7c7c32c2afc2a4be5d4455bd42ba1f5a4c
Nic Mortelliti <npmortelliti@gmail.com>
2023-11-03 15:03:06 -0700

    Revert "Merge commit 'd96440ed7031eb6df4cfeb79d2b96ea701f1214a'"
    This reverts commit f1fc2adc576449dee4fb1df4a1ee5a4ae72b6afd, reversing
    changes made to 5b0909119038f468152e9c1bf175a286291444af.

---
+++ app/models/bad_pass.rb
@@ -0,0 +1,2 @@
+class BadPass < ApplicationRecord
+end
---
+++ test/models/bad_pass_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BadPassTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
